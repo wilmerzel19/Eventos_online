7d2fcff376ea807530ceff5921c64dc69d3cfea8
wilmer <est.wzelayam@uml.edu.ni>
2023-03-20 17:50:14 -0600

    Diseño: Integracion de Flowbite y barra de navegación inferior

---
+++ vite.config.cjs
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+const { resolve } = require('path')
+//Add env variable to check if is production or development
+
+const env = process.env;
+//Set a base route if deploying on github pages
+//env.mode = env.mode || "production";
+
+console.log('Environment: ', env.mode);
+
+export default defineConfig({
+  root: "./",
+  //Add base route if deployiing on production
+  base: env.mode === "production" ? "/app/" : "/",
+  build: {
+    outDir: "docs",
+    assetsDir: "assets",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  },
+  plugins: []
+});
