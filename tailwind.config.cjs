3bf8b71c8fdc74b55f162099bd9bd453c234881d
wilmer <est.wzelayam@uml.edu.ni>
2023-03-24 12:08:32 -0600

    home

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}","*.html","*.css",   "./node_modules/flowbite/**/*.js"
+  content: ["./src/**/*.{html,js}","*.html","*.css",   "./node_modules/flowbite/**/*.js",
+  darkMode = 'class'// Esta linea se agrega para que tailwind cargue el modo oscuro
+  
+
  ], // Esta linea se agrega para que tailwind cargue la paquete Flowbite
   theme: {
     extend: {},
